07116c5aeda1bb246d42572153dc146967570bbb
sxjjbhcc <82585678+sxjjbhcc@users.noreply.github.com>
2022-08-23 23:19:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,10 +35,8 @@ def get_birthday():
   return (next - today).days
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+  words = requests.get("https://api.yixiangzhilv.com/yiyan/sentence/")
+  return words.json()['content']
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
